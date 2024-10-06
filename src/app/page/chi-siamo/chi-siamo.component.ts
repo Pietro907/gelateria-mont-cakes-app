056cea7c3c7196337e71733dc70eabb363bb614e
Pietro907 <pietrop.puri@gmail.com>
2024-10-07 01:29:41 +0200

    add fix  header sticky & footer in section in page chi_siamo

--- src/app/page/chi-siamo/chi-siamo.component.ts
+++ src/app/page/chi-siamo/chi-siamo.component.ts
@@ -13,22 +13,44 @@ export class ChiSiamoComponent implements AfterViewInit {
     items.forEach((item) => {
       if (item.isIntersecting) {
         item.target.classList.add('in_page');
+
       } else {
         item.target.classList.remove('in_page');
       }
     });
   };
 
-  observer: IntersectionObserver = new IntersectionObserver(this.callback, {threshold: 0.9});
+  observer: IntersectionObserver = new IntersectionObserver(this.callback, {threshold: 0.6});
   
   ngAfterViewInit(){
-    
+    const elHeader = document.getElementById('header_app') as HTMLElement;
+    elHeader.style.display="none";
+    const elFooter = document.getElementById('footer_app') as HTMLElement;
+    elFooter.style.display="none";
+
+    const elHeaderChi = document.getElementById('header_chi_siamo') as HTMLElement;
+
+    elHeaderChi.style.position='sticky';
+    elHeaderChi.style.top='-151px';
+    elHeaderChi.style.zIndex='2';
+
     const elSection = document.querySelectorAll(".section");
   
     elSection.forEach((element: Element) =>{
     this.observer.observe(element)
-    
+    this.observer.observe(elHeader)
+
     });
+
+    const elPreTopMenu = document.getElementById('pre_top_header');
+    const elPreTopMenu2 = document.getElementById('pre_top_header2');
+    
+    console.log(elPreTopMenu2);
+
+    elPreTopMenu!.style.background='#CCAA00';
+    elPreTopMenu2!.style.background='#CCAA00';
+
+  
   }
 }
 
