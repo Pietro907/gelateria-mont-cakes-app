1f2e1fd42049b8e635bd6c80d93aa0a243e39314
Pietro907 <pietrop.puri@gmail.com>
2024-10-11 19:11:29 +0200

    add rework page chi_siamo

--- src/app/page/chi-siamo/chi-siamo.component.ts
+++ src/app/page/chi-siamo/chi-siamo.component.ts
@@ -1,56 +1,46 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
-import { Observer } from 'rxjs';
+import { AfterViewInit, Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-chi-siamo',
   templateUrl: './chi-siamo.component.html',
-  styleUrl: './chi-siamo.component.css'
+  styleUrls: ['./chi-siamo.component.css', './chi-siamo-section.component.css', './chi-siamo-utilities.component.css']
 })
 export class ChiSiamoComponent implements AfterViewInit {
-
-  
   callback: IntersectionObserverCallback = (items) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
         item.target.classList.add('in_page');
-
+        
+                
       } else {
-        item.target.classList.remove('in_page');
+        item.target.classList.remove('in_page');        
+        
       }
     });
   };
 
-  observer: IntersectionObserver = new IntersectionObserver(this.callback, {threshold: 0.6});
-  
-  ngAfterViewInit(){
-    const elHeader = document.getElementById('header_app') as HTMLElement;
-    elHeader.style.display="none";
-    const elFooter = document.getElementById('footer_app') as HTMLElement;
-    elFooter.style.display="none";
+  observer: IntersectionObserver = new IntersectionObserver(this.callback, {
+    threshold: 0.6
+  });
 
-    const elHeaderChi = document.getElementById('header_chi_siamo') as HTMLElement;
 
-    elHeaderChi.style.position='sticky';
-    elHeaderChi.style.top='-151px';
-    elHeaderChi.style.zIndex='2';
+  ngAfterViewInit() {
+ 
 
-    const elSection = document.querySelectorAll(".section");
-  
-    elSection.forEach((element: Element) =>{
-    this.observer.observe(element)
-    this.observer.observe(elHeader)
+    const elSection = document.querySelectorAll('.section');
 
+    elSection.forEach((element: Element) => {
+      this.observer.observe(element);
+      
     });
 
     const elPreTopMenu = document.getElementById('pre_top_header');
     const elPreTopMenu2 = document.getElementById('pre_top_header2');
-    
+
     console.log(elPreTopMenu2);
 
-    elPreTopMenu!.style.background='#CCAA00';
-    elPreTopMenu2!.style.background='#CCAA00';
-
-  
+    elPreTopMenu!.style.background = '#CCAA00';
+    elPreTopMenu2!.style.background = '#CCAA00';
   }
 }
-
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+      const elPreTopMenu = document.getElementById('pre_top_header');
+      const elPreTopMenu2 = document.getElementById('pre_top_header2');
+      
+      console.log(elPreTopMenu2);
+  
+      elPreTopMenu!.style.background='#CCAA00';
+      elPreTopMenu2!.style.background='#CCAA00';
 
+        const elHeader = document.getElementById('header_app') as HTMLElement;
+        elHeader.style.display="none";
+        const elFooter = document.getElementById('footer_app') as HTMLElement;
+        elFooter.style.display="none";
+    
+        const elHeaderHome = document.getElementById('header_home') as HTMLElement;
+    
+        elHeaderHome.style.position='sticky';
+        elHeaderHome.style.top='-151px';
+        elHeaderHome.style.zIndex='2';
+  }
 }
